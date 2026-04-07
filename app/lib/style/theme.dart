3ac31ee7e0862e82ec462a4dd9dff90a099bace8
renz0826 <brenzbedonia@gmail.com>
2026-04-07 22:02:25 +0800

    feat: create and apply text themes

--- app/lib/style/theme.dart
+++ app/lib/style/theme.dart
@@ -22,6 +22,42 @@ class AppTheme {
   static const Color yellow200 = Color(0xFFFFF9C4);
   static const Color yellow600 = Color(0xFFFFC107);
 
+  // text styles
+  static const TextTheme textTheme = TextTheme(
+    displayLarge: TextStyle(
+      fontSize: 60,
+      fontWeight: FontWeight.w500,
+      color: green500,
+    ),
+    headlineLarge: TextStyle(
+      fontSize: 32,
+      fontWeight: FontWeight.w500,
+      color: black500,
+    ),
+    titleLarge: TextStyle(
+      fontSize: 24,
+      fontWeight: FontWeight.w600,
+      color: black500,
+    ),
+    bodyLarge:
+        TextStyle(fontSize: 16, fontWeight: FontWeight.w600, color: black500),
+    bodyMedium: TextStyle(
+      fontSize: 16,
+      fontWeight: FontWeight.normal,
+      color: black500,
+    ),
+    bodySmall: TextStyle(
+      fontSize: 14,
+      fontWeight: FontWeight.w400,
+      color: gray500,
+    ),
+    labelMedium: TextStyle(
+      fontSize: 16,
+      fontWeight: FontWeight.w600,
+      color: white500,
+    ),
+  );
+
   // theme application
   static ThemeData get lightTheme {
     return ThemeData(
@@ -30,9 +66,10 @@ class AppTheme {
           seedColor: green500,
           primary: green500,
           onPrimary: white500,
-          surface: white500,
+          surface: gray200,
           error: red600,
         ),
+        textTheme: textTheme,
         scaffoldBackgroundColor: gray200);
   }
 }
