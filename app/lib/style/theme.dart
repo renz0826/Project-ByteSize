f25048cf770168a8fca602545d50e488fd4e992a
renz0826 <brenzbedonia@gmail.com>
2026-04-07 21:13:36 +0800

    feat: import primitive colors

---
+++ app/lib/style/theme.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  // app color scheme
+
+  // greens
+  static const Color green500 = Color(0x388037);
+  static const Color green400 = Color(0x48A447);
+  static const Color green300 = Color(0x4FB44E);
+  static const Color green700 = Color(0x214C21);
+  static const Color green200 = Color(0xC5FFC5);
+
+  // neutrals
+  static const Color white500 = Color(0xFFFFFF);
+  static const Color gray200 = Color(0xFDFDFD);
+  static const Color gray400 = Color(0xB5B5B5);
+  static const Color gray500 = Color(0x545454);
+  static const Color black500 = Color(0x000000);
+
+  // status
+  static const Color red600 = Color(0xB50000);
+  static const Color yellow200 = Color(0xFFF9C4);
+  static const Color yellow600 = Color(0xFFC107);
+}
