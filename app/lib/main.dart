cc1d5b74c9520e1d3c2f31c0faad04ff6657c26b
renz0826 <brenzbedonia@gmail.com>
2026-04-09 19:33:14 +0800

    feat: integrate main layout screen

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:sqflite_common_ffi/sqflite_ffi.dart'; // Required for Windows DB
+
 import 'db/database.dart';
+import 'style/theme.dart';
+import 'screens/main_layout.dart';
 
 final dbProvider = Provider<AppDatabase>((ref) => AppDatabase());
 
-void main() { // test code lang ni, just to check if shit is working
+void main() {
+  // Required to prevent the Windows engine from crashing on startup
+  WidgetsFlutterBinding.ensureInitialized();
+  databaseFactory = databaseFactoryFfi;
+
+  // test code lang ni, just to check if shit is working
   runApp(const ProviderScope(child: MyApp()));
 }
 
@@ -15,14 +24,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'DentCity',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
-      ),
-      home: const Scaffold(
-        body: Center(
-          child: Text('DentCity is running!'),
-        ),
-      ),
+      theme: AppTheme.lightTheme,
+      home: const MainLayoutScreen(),
     );
   }
 }
