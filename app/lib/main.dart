3372ac1576d219bf257a5e1a334903bfd10d27f5
fonsfons28 <business.alfonsodolutan@gmail.com>
2026-04-22 18:41:44 +0800

    test: added poke query
    allows to create database on your own devices

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -3,14 +3,30 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'pages/login_page.dart';
 import 'pages/main_layout.dart';
+import 'db/database.dart';
 
-void main() {
+void main() async {
+  
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final database = AppDatabase(); // call database from the db file
+
+  try { // Poke Query, this is used to create the db file on your documents tab (gin ai ko lang ni for testing)
+    await database.customSelect('SELECT 1').getSingle();
+    debugPrint("Database has been created");
+  } catch (e) {
+    debugPrint("Database creation failed: $e");
+  }
+
+  // 4. Start the app
   runApp(
     const ProviderScope(
       child: DentalApp(),
     ),
   );
 }
+
+
 
 class DentalApp extends StatelessWidget {
   const DentalApp({super.key});
