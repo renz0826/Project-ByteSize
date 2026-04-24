d58c91bf3eb0e442af8290b651f58fc1f8281b3b
fonsfons28 <business.alfonsodolutan@gmail.com>
2026-04-24 10:28:33 +0800

    fix: changed to providers, no more controllers

--- app/lib/pages/login_page.dart
+++ app/lib/pages/login_page.dart
@@ -2,7 +2,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import '../controllers/auth_controller.dart';
+import '../providers/auth_provider.dart';
 import '../widgets/horizontal_logo.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import '../widgets/main_buttons.dart';
