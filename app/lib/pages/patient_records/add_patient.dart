59779486cd3f7e7d698294b7c9f768d4ce280ee3
renz0826 <brenzbedonia@gmail.com>
2026-04-27 14:08:31 +0800

    feat: migrate patient personal info form

---
+++ app/lib/pages/patient_records/add_patient.dart
@@ -0,0 +1,237 @@
+import 'package:dentcity_management_system/style/theme.dart';
+import 'package:flutter/material.dart';
+import '/../widgets/main_buttons.dart';
+import '/../widgets/input_field.dart';
+
+class AddPatientForm extends StatefulWidget {
+  final VoidCallback onNext;
+
+  const AddPatientForm({super.key, required this.onNext});
+
+  @override
+  State<AddPatientForm> createState() => _AddPatientFormState();
+}
+
+class _AddPatientFormState extends State<AddPatientForm> {
+  String? _defaultSelection;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(24),
+      padding: const EdgeInsets.all(32),
+      decoration: BoxDecoration(
+        color: AppTheme.white500,
+        borderRadius: BorderRadius.circular(24),
+        boxShadow: AppTheme.floatShadow,
+      ),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text("Personal Details",
+              style: Theme.of(context).textTheme.headlineLarge),
+
+          const SizedBox(height: 22),
+
+          // --- FULL NAME ---
+          Text("Full Name", style: Theme.of(context).textTheme.titleLarge),
+          const SizedBox(height: 12),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Expanded(
+                  child: InputField(
+                      label: "First Name", hintText: "Enter first name")),
+              const SizedBox(width: 20),
+              Expanded(
+                  child: InputField(
+                      label: "Middle Name", hintText: "Enter middle name")),
+              const SizedBox(width: 20),
+              Expanded(
+                  child: InputField(
+                      label: "Last Name", hintText: "Enter last name")),
+            ],
+          ),
+
+          const SizedBox(height: 22),
+
+          // --- DEMOGRAPHIC ---
+          Text("Demographic", style: Theme.of(context).textTheme.titleLarge),
+          const SizedBox(height: 12),
+          SizedBox(
+            width: 800,
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(
+                    child: InputField(
+                        hintText: "Select a month",
+                        label: "Month",
+                        variant: InputVariant.dropdown,
+                        dropdownValue: _defaultSelection,
+                        dropdownItems: const [
+                      "January",
+                      "February",
+                      "March",
+                      "April",
+                      "May"
+                    ])),
+                const SizedBox(width: 20),
+                Expanded(
+                    child: InputField(
+                        hintText: "Select a day",
+                        label: "Day",
+                        variant: InputVariant.dropdown,
+                        dropdownValue: _defaultSelection,
+                        dropdownItems: const ["1", "2", "3", "4", "5"])),
+                const SizedBox(width: 20),
+                Expanded(
+                    child: InputField(
+                        hintText: "Select a year",
+                        label: "Year",
+                        variant: InputVariant.dropdown,
+                        dropdownValue: _defaultSelection,
+                        dropdownItems: const ["2001", "2002", "2006"])),
+              ],
+            ),
+          ),
+          const SizedBox(height: 18),
+          SizedBox(
+            width: 525,
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(
+                    child: InputField(
+                        hintText: "Select a sex",
+                        label: "Sex",
+                        variant: InputVariant.dropdown,
+                        dropdownValue: _defaultSelection,
+                        dropdownItems: const ["Male", "Female"])),
+                const SizedBox(width: 20),
+                Expanded(
+                    child: InputField(
+                        hintText: "Select a civil status",
+                        label: "Civil Status",
+                        variant: InputVariant.dropdown,
+                        dropdownValue: _defaultSelection,
+                        dropdownItems: const [
+                      "Single",
+                      "Married",
+                      "Widowed",
+                      "Annulled"
+                    ])),
+              ],
+            ),
+          ),
+
+          const SizedBox(height: 22),
+
+          // --- CONTACT INFO ---
+          Text("Contact Information",
+              style: Theme.of(context).textTheme.titleLarge),
+          const SizedBox(height: 12),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Expanded(
+                  child: InputField(
+                      label: "Mobile Number", hintText: "Enter mobile number")),
+              const SizedBox(width: 20),
+              Expanded(
+                  child: InputField(
+                      label: "Emergency Contact Number",
+                      hintText: "Enter emergency number")),
+            ],
+          ),
+          const SizedBox(height: 18),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Expanded(
+                  child: InputField(
+                      label: "Referred By", hintText: "Enter referral")),
+              const SizedBox(width: 20),
+              Expanded(
+                  child: InputField(
+                      label: "Relationship",
+                      hintText: "Relationship with referral")),
+            ],
+          ),
+
+          const SizedBox(height: 22),
+
+          // --- ADDRESS ---
+          Text("Address", style: Theme.of(context).textTheme.titleLarge),
+          const SizedBox(height: 12),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Expanded(
+                  child: InputField(
+                      hintText: "Select a Street",
+                      label: "Street Address",
+                      variant: InputVariant.dropdown,
+                      dropdownValue: _defaultSelection,
+                      dropdownItems: const ["Luna St."])),
+              const SizedBox(width: 20),
+              Expanded(
+                  child: InputField(
+                      hintText: "Select a Barangay",
+                      label: "Barangay",
+                      variant: InputVariant.dropdown,
+                      dropdownValue: _defaultSelection,
+                      dropdownItems: const ["Brgy. Magsaysay"])),
+              const SizedBox(width: 20),
+              Expanded(
+                  child: InputField(
+                      hintText: "Select a City/Municipality",
+                      label: "City/Municipality",
+                      variant: InputVariant.dropdown,
+                      dropdownValue: _defaultSelection,
+                      dropdownItems: const ["La Paz, Iloilo City"])),
+              const SizedBox(width: 20),
+              Expanded(
+                  child: InputField(
+                      hintText: "Select a Province",
+                      label: "Province",
+                      variant: InputVariant.dropdown,
+                      dropdownValue: _defaultSelection,
+                      dropdownItems: const ["Iloilo"])),
+              const SizedBox(width: 20),
+              SizedBox(
+                width: 150,
+                child: InputField(
+                    hintText: "Select a ZIP",
+                    label: "ZIP Code",
+                    variant: InputVariant.dropdown,
+                    dropdownValue: _defaultSelection,
+                    dropdownItems: const ["5000"]),
+              ),
+            ],
+          ),
+
+          const SizedBox(height: 32),
+
+          // --- ACTION BUTTON ---
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              SizedBox(
+                width: 160,
+                child: Button(
+                  label: "Next",
+                  width: double.infinity,
+                  icon: Icons.arrow_forward,
+                  iconPlacement: IconPlacement.right,
+                  onPressed: widget.onNext,
+                ),
+              )
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
