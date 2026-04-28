a3922f77f89a6138054e26c54ad9b611029fe48e
renz0826 <brenzbedonia@gmail.com>
2026-04-28 10:53:44 +0800

    feat: add clinical record form

--- app/lib/pages/patient_records/add_clinical_record.dart
+++ app/lib/pages/patient_records/add_clinical_record.dart
@@ -13,16 +13,300 @@ class AddClinicalRecordForm extends StatefulWidget {
 }
 
 class _AddClinicalRecordFormState extends State<AddClinicalRecordForm> {
+  String? _defaultSelection;
+  int _teethExtracted = 0;
+
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: 160,
-      child: Button(
-        label: "Next",
-        width: double.infinity,
-        icon: Icons.arrow_forward,
-        iconPlacement: IconPlacement.right,
-        onPressed: widget.onFinish,
+    return Center(
+      child: Container(
+        constraints: const BoxConstraints(maxWidth: 1200),
+        margin: const EdgeInsets.all(24),
+        padding: const EdgeInsets.all(32),
+        decoration: BoxDecoration(
+          color: AppTheme.white500,
+          borderRadius: BorderRadius.circular(24),
+          boxShadow: AppTheme.floatShadow,
+        ),
+        child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text("Clinical Record",
+                  style: Theme.of(context).textTheme.headlineLarge),
+
+              const SizedBox(height: 32),
+
+              // -- MEDICAL RECORD --
+              Text("Medical Background",
+                  style: Theme.of(context).textTheme.titleLarge),
+
+              const SizedBox(height: 12),
+
+              Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  // Row 1: Past and Present Illnesses
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Expanded(
+                          child: InputField(
+                              label: "Past Illnesses",
+                              hintText: "Enter patient's past illnesses")),
+                      const SizedBox(width: 20),
+                      Expanded(
+                          child: InputField(
+                              label: "Present Illnesses",
+                              hintText: "Enter patient's present illnesses")),
+                    ],
+                  ),
+
+                  const SizedBox(height: 20),
+
+                  // Row 2: Allergies and Medication
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Expanded(
+                          child: InputField(
+                              label: "Allergies",
+                              hintText: "Enter patient's allergies")),
+                      const SizedBox(width: 20),
+                      Expanded(
+                          child: InputField(
+                              label: "Current Medication",
+                              hintText: "Enter patient's current medication")),
+                    ],
+                  ),
+
+                  const SizedBox(height: 32),
+
+                  // -- TREATMENT PROCEDURE --
+
+                  Text("Treatment Procedure",
+                      style: Theme.of(context).textTheme.titleLarge),
+
+                  const SizedBox(height: 12),
+
+                  Column(
+                      mainAxisSize: MainAxisSize.min,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        // Row 1: Oral Debris, Calculus, and Gingivitis
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            RadioGroupField(
+                              label: "Presence of Oral Debris",
+                              options: const ["Present", "None"],
+                              selectedValue: _defaultSelection,
+                              onChanged: (String value) {
+                                setState(() {
+                                  _defaultSelection = value;
+                                });
+                              },
+                            ),
+                            const SizedBox(width: 100),
+                            RadioGroupField(
+                              label: "Presence of Oral Debris",
+                              options: const ["Present", "None"],
+                              selectedValue: _defaultSelection,
+                              onChanged: (String value) {
+                                setState(() {
+                                  _defaultSelection = value;
+                                });
+                              },
+                            ),
+                            const SizedBox(width: 100),
+                            RadioGroupField(
+                              label: "Presence of Oral Debris",
+                              options: const ["Present", "None"],
+                              selectedValue: _defaultSelection,
+                              onChanged: (String value) {
+                                setState(() {
+                                  _defaultSelection = value;
+                                });
+                              },
+                            ),
+                          ],
+                        ),
+
+                        const SizedBox(height: 20),
+
+                        // Row 2: Periodontal Pocket and Dentofacial Anomaly
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            RadioGroupField(
+                              label: "Presence of Periodontal Pocket",
+                              options: const ["Present", "None"],
+                              selectedValue: _defaultSelection,
+                              onChanged: (String value) {
+                                setState(() {
+                                  _defaultSelection = value;
+                                });
+                              },
+                            ),
+                            const SizedBox(width: 64),
+                            RadioGroupField(
+                              label: "Presence of Dentofacial Anomaly",
+                              options: const ["Present", "None"],
+                              selectedValue: _defaultSelection,
+                              onChanged: (String value) {
+                                setState(() {
+                                  _defaultSelection = value;
+                                });
+                              },
+                            ),
+                          ],
+                        ),
+                      ]),
+
+                  const SizedBox(height: 32),
+
+                  // -- TOOTH COUNT --
+
+                  Text("Tooth Count",
+                      style: Theme.of(context).textTheme.titleLarge),
+
+                  const SizedBox(height: 12),
+
+                  SizedBox(
+                    width: 800,
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        // Row 1: Filling, Extraction, and Root Fragment
+                        Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Expanded(
+                              child: InputField(
+                                label: "Caries Indicated for Filling",
+                                variant: InputVariant.counter,
+                                counterValue: _teethExtracted,
+                                counterMin: 0,
+                                counterMax: 32,
+                                onCounterChanged: (newValue) {
+                                  setState(() {
+                                    _teethExtracted = newValue;
+                                  });
+                                },
+                              ),
+                            ),
+                            const SizedBox(width: 64),
+                            Expanded(
+                              child: InputField(
+                                label: "Caries Indicated for Extraction",
+                                variant: InputVariant.counter,
+                                counterValue: _teethExtracted,
+                                counterMin: 0,
+                                counterMax: 32,
+                                onCounterChanged: (newValue) {
+                                  setState(() {
+                                    _teethExtracted = newValue;
+                                  });
+                                },
+                              ),
+                            ),
+                            const SizedBox(width: 64),
+                            Expanded(
+                              child: InputField(
+                                label: "Root Fragment",
+                                variant: InputVariant.counter,
+                                counterValue: _teethExtracted,
+                                counterMin: 0,
+                                counterMax: 32,
+                                onCounterChanged: (newValue) {
+                                  setState(() {
+                                    _teethExtracted = newValue;
+                                  });
+                                },
+                              ),
+                            ),
+                          ],
+                        ),
+
+                        const SizedBox(height: 20),
+
+                        // Row 2: Missing and Filled or Restored
+                        SizedBox(
+                          width: 510,
+                          child: Row(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Expanded(
+                                child: InputField(
+                                  label: "Missing Due to Caries",
+                                  variant: InputVariant.counter,
+                                  counterValue: _teethExtracted,
+                                  counterMin: 0,
+                                  counterMax: 32,
+                                  onCounterChanged: (newValue) {
+                                    setState(() {
+                                      _teethExtracted = newValue;
+                                    });
+                                  },
+                                ),
+                              ),
+                              const SizedBox(width: 64),
+                              Expanded(
+                                child: InputField(
+                                  label: "Filled or Restored",
+                                  variant: InputVariant.counter,
+                                  counterValue: _teethExtracted,
+                                  counterMin: 0,
+                                  counterMax: 32,
+                                  onCounterChanged: (newValue) {
+                                    setState(() {
+                                      _teethExtracted = newValue;
+                                    });
+                                  },
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+
+                  const SizedBox(height: 32),
+
+                  // -- TREATMENT PLAN --
+
+                  Text("Treatment Plan",
+                      style: Theme.of(context).textTheme.titleLarge),
+
+                  const SizedBox(height: 12),
+
+                  InputField(
+                    hintText:
+                        "Write important clinical notes during the appointment",
+                    label: "Clinical Notes",
+                    maxLines: 10,
+                  ),
+
+                  const SizedBox(height: 32),
+
+                  Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Button(
+                          label: "Next",
+                          width: 300,
+                          icon: Icons.arrow_forward,
+                          iconPlacement: IconPlacement.right,
+                          onPressed: widget.onFinish,
+                        ),
+                      ])
+                ],
+              ),
+            ]),
       ),
     );
   }
