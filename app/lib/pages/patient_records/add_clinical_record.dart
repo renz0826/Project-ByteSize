d743c4e94293c887f463dd593cab3741fa5f7a52
renz0826 <brenzbedonia@gmail.com>
2026-04-28 08:35:01 +0800

    feat: create base stateful widget for add clinical record

---
+++ app/lib/pages/patient_records/add_clinical_record.dart
@@ -0,0 +1,19 @@
+import 'package:dentcity_management_system/style/theme.dart';
+import 'package:flutter/material.dart';
+import '/../widgets/main_buttons.dart';
+import '/../widgets/input_field.dart';
+import '/../widgets/radio_buttons.dart';
+
+class AddClinicalRecordForm extends StatefulWidget {
+  const AddClinicalRecordForm({super.key});
+
+  @override
+  State<AddClinicalRecordForm> createState() => _AddClinicalRecordFormState();
+}
+
+class _AddClinicalRecordFormState extends State<AddClinicalRecordForm> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
