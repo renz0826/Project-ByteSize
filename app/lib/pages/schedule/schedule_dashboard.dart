b804bb4b0f42f25d0fbf1517e4d2fc3e72f74f1a
renz0826 <brenzbedonia@gmail.com>
2026-04-30 18:58:26 +0800

    feat: create schedules dashBoard states

--- app/lib/pages/schedule/schedule_dashboard.dart
+++ app/lib/pages/schedule/schedule_dashboard.dart
@@ -1,10 +1,73 @@
+import 'package:dentcity_management_system/pages/patient_records/add_clinical_record.dart';
 import 'package:flutter/material.dart';
+import '/../widgets/main_buttons.dart';
+import 'schedule_appointment.dart';
 
-class ScheduleDashboard extends StatelessWidget {
+// enum of different schedule views
+// TODO: Add different Edit and View Patient Record Views
+enum SchedulesView {
+  main,
+  scheduleAppointment,
+}
+
+class ScheduleDashboard extends StatefulWidget {
   const ScheduleDashboard({super.key});
 
   @override
+  State<ScheduleDashboard> createState() => _ScheduleDashboardState();
+}
+
+class _ScheduleDashboardState extends State<ScheduleDashboard> {
+  // Functions to change schedule screen states
+  SchedulesView _currentView = SchedulesView.main;
+
+  void _goToScheduleAppointment() {
+    setState(() => _currentView = SchedulesView.scheduleAppointment);
+  }
+
+  // TODO: Make the save button functional and return to the main dashboard.
+  void _goBackToMain() {
+    setState(() => _currentView = SchedulesView.main);
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    Widget activeScreen;
+
+    // Switches the view for each screen
+    // TODO: Add View and Edit States
+    switch (_currentView) {
+      case SchedulesView.scheduleAppointment:
+        activeScreen = ScheduleAppointmentForm(
+          onSave: _goBackToMain,
+        );
+      case SchedulesView.main:
+        activeScreen = _buildMainDashboard();
+        break;
+    }
+
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: activeScreen,
+        ),
+      ),
+    );
+  }
+
+  Widget _buildMainDashboard() {
+    return Container(
+      padding: const EdgeInsets.all(24),
+      child: Column(
+        children: [
+          const SizedBox(height: 20),
+          Button(
+              label: "Schedule Patient",
+              variant: ButtonVariant.primary,
+              width: double.infinity,
+              onPressed: _goToScheduleAppointment),
+        ],
+      ),
+    );
   }
 }
