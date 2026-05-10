7e28e58271c87e9798f88c5d69577fbd4837a4cb
fonsfons28 <business.alfonsodolutan@gmail.com>
2026-05-10 23:38:36 +0800

    fix: edited getBookedSlots

--- app/lib/repositories/appointment_repository.dart
+++ app/lib/repositories/appointment_repository.dart
@@ -46,16 +46,21 @@ class AppointmentRepository {
   Future<int> deleteAppointment(int id) => 
     (db.delete(db.appointment) ..where ((a) => a.appointmentId.equals(id))) .go(); 
 
-  // --- NEW: SCAN FOR BOOKED SLOTS ---
+  
+  // --- NEW: SCAN FOR BOOKED SLOTS (Ignores Cancelled!) ---
   Future<List<String>> getBookedSlots(DateTime date) async {
     final startOfDay = DateTime(date.year, date.month, date.day, 0, 0, 0);
     final endOfDay = DateTime(date.year, date.month, date.day, 23, 59, 59);
 
-    final query = db.select(db.appointment)
-      // Using scheduleDateTime to match your schema
-      ..where((a) => a.scheduleDateTime.isBetweenValues(startOfDay, endOfDay)); 
-    
-    final results = await query.get();
-    return results.map((a) => a.timeSlot).toList();
+    final appointments = await (db.select(db.appointment)
+          ..where((a) => a.scheduleDateTime.isBetweenValues(startOfDay, endOfDay))
+          // THIS IS THE MAGIC LINE: Ignore cancelled appointments!
+          ..where((a) => a.status.isNotValue('Cancelled'))) 
+        .get();
+
+    // Return the list of time slots that are actually taken
+    return appointments.map((a) => a.timeSlot).whereType<String>().toList();
   }
+
+  
 }
