8d51bbcd3db045d921a3c1853b01998f725d8c5f
fonsfons28 <business.alfonsodolutan@gmail.com>
2026-05-11 08:46:22 +0800

    fix: imports

--- app/lib/pages/patient_records/patient_dashboard.dart
+++ app/lib/pages/patient_records/patient_dashboard.dart
@@ -1,3 +1,4 @@
+import 'package:dentcity_management_system/widgets/status_toast.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:drift/drift.dart' as drift;
