9cf27678ad61995dcae47387c408deacfac3b211
Dawn <merii.dunn@gmail.com>
2026-05-14 16:11:47 +0800

    feat: add current PIN verification before allowing PIN update in ProfilePage

--- app/lib/pages/profile/profile_page.dart
+++ app/lib/pages/profile/profile_page.dart
@@ -3,10 +3,10 @@ import '../../style/theme.dart';
 import '../../widgets/page_header.dart';
 import '../../widgets/input_field.dart'; 
 import '../../widgets/main_buttons.dart'; 
-import 'package:heroicons/heroicons.dart';
+import '../../widgets/status_toast.dart';
 
 class ProfilePage extends StatefulWidget {
-  const ProfilePage({super.key});
+  const ProfilePage({super.key,});
 
   @override
   State<ProfilePage> createState() => _ProfilePageState();
@@ -23,6 +23,17 @@ class _ProfilePageState extends State<ProfilePage> {
   final _newPinController = TextEditingController();
   final _confirmPinController = TextEditingController();
 
+  bool _isCurrentPinObscured = true;
+  bool _isNewPinObscured = true;
+  bool _isConfirmPinObscured = true;
+
+ // Pin Validation State
+  bool _isCurrentPinVerified = false;
+  bool _isPinError = false;
+  String _pinErrorMessage = '';
+
+  static const String _correctPin = '1234';
+
   @override
   void dispose() {
     // Clean up controllers when the page is closed
@@ -33,6 +44,22 @@ class _ProfilePageState extends State<ProfilePage> {
     _newPinController.dispose();
     _confirmPinController.dispose();
     super.dispose();
+  }
+
+  void _verifyCurrentPin(){
+    final entered = _currentPinController.text.trim();
+    if (entered == _correctPin){
+      setState(() {
+        _isCurrentPinVerified = true;
+        _isPinError = false;
+        _pinErrorMessage = '';
+      });
+    } else {
+      setState(() {
+        _isPinError = true;
+        _pinErrorMessage = 'Incorrect PIN. Please try again.';
+      });
+    }
   }
   
   // Reusable card 
@@ -51,10 +78,7 @@ class _ProfilePageState extends State<ProfilePage> {
         children: [
           Text(
             title,
-            style: AppTheme.textTheme.headlineLarge?.copyWith(
-              fontSize: 24,
-              fontWeight: FontWeight.w600
-            ),
+            style: AppTheme.textTheme.headlineLarge,
           ),
           const SizedBox(height: 32),
           ...children,
@@ -68,20 +92,22 @@ class _ProfilePageState extends State<ProfilePage> {
     return Scaffold(
       backgroundColor: AppTheme.gray200, 
       body: SingleChildScrollView(
-        child: Center(
-          child: Container(
-            constraints: const BoxConstraints(maxWidth: 1200),
-            padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 24),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const PageHeader(
-                  title: 'Account Settings',
-                  type: PageHeaderType.plain,
-                ),
+        child: Column(
+          children: [
+            const PageHeader(
+              title: 'Account Settings',
+              type: PageHeaderType.plain,
+            ),
 
-                const SizedBox(height: 32),
-                  
+            Center(
+              child: Container(
+              constraints: const BoxConstraints(maxWidth: 1200),
+              padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 24),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(height: 8),
+                
                 // Personal Info Card
                 _buildSectionCard(
                   title: "Personal Information", 
@@ -111,7 +137,7 @@ class _ProfilePageState extends State<ProfilePage> {
                           child: Button(
                             label: "Update Profile",
                             variant: ButtonVariant.primary,
-                            heroIcon: HeroIcons.check,
+                            icon: Icons.check,
                             onPressed: () {} // TO DO: Logic in updating Profile
                           ),
                         ),
@@ -121,7 +147,7 @@ class _ProfilePageState extends State<ProfilePage> {
                   // Security and Access Card
                   _buildSectionCard(
                     title: "Security and Access",
-                     children: [
+                    children: [
                       Text("Update Login PIN",
                         style: AppTheme.textTheme.bodyLarge),
                         const SizedBox(height: 24),
@@ -129,44 +155,125 @@ class _ProfilePageState extends State<ProfilePage> {
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
                             Expanded(
-                              child: InputField(label: "Current PIN",
-                              controller: _currentPinController,
-                              obscureText: true
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  InputField(label: "Current PIN",
+                                  controller: _currentPinController,
+                                  obscureText: _isCurrentPinObscured,
+                                  suffixIcon: IconButton(
+                                    icon: Icon(_isCurrentPinObscured ? Icons.visibility_off : Icons.visibility),
+                                    onPressed: () => setState(
+                                      () => _isCurrentPinObscured = !_isCurrentPinObscured),
+                                  ),
+                                ),
+                                if (_isPinError)...[
+                                  const SizedBox(height: 6),
+                                  Text(
+                                    _pinErrorMessage,
+                                    style: AppTheme.textTheme.bodySmall?.copyWith(
+                                      color: AppTheme.red600),
+                                  ),
+                                ],
+                                if (_isCurrentPinVerified)...[
+                                  const SizedBox(height: 6),
+                                  Text(
+                                    'Pin Verified',
+                                    style: AppTheme.textTheme.bodySmall?.copyWith(
+                                      color: AppTheme.green300
+                                    )
+                                  )
+                                ]
+                                ],
                               )
-                            ),
+                          ),
                             const SizedBox(width: 20),
                             Expanded(
-                              child: InputField(label: "New PIN",
-                              controller: _newPinController,
-                              obscureText: true
-                              )
+                              child: Opacity(
+                                opacity: _isCurrentPinVerified ? 1.0 : 0.4,
+                                child: IgnorePointer(
+                                  ignoring: !_isCurrentPinVerified,
+                                  child: InputField(label: "New PIN",
+                                  controller: _newPinController,
+                                  obscureText: _isNewPinObscured,
+                                  suffixIcon: IconButton(
+                                    onPressed: () {
+                                      setState(() {
+                                        _isNewPinObscured = !_isNewPinObscured;
+                                      });
+                                    }, 
+                                    icon: Icon(_isNewPinObscured ? Icons.visibility_off : Icons.visibility)),
+                                  )
+                                ),
+                              ),
                             ),
+
+
                             const SizedBox(width: 20),
                             Expanded(
-                              child: InputField(label: "Confirm New PIN",
-                              controller: _confirmPinController,
-                              obscureText: true
-                              )
+                              child: Opacity(
+                                opacity: _isCurrentPinVerified ? 1.0 : 0.4,
+                                child: IgnorePointer(
+                                  ignoring: !_isCurrentPinVerified,
+                                  child: InputField(
+                                  label: "Confirm New PIN",
+                                  controller: _confirmPinController,
+                                  obscureText: _isConfirmPinObscured,
+                                  suffixIcon: IconButton(
+                                    onPressed: (){
+                                      setState(() {
+                                        _isConfirmPinObscured = !_isConfirmPinObscured;
+                                      });
+                                    }, 
+                                    icon: Icon(_isConfirmPinObscured ? Icons.visibility_off : Icons.visibility)),
+                                  )
+                                ),
+                              ),
                             ),
                           ],
                         ),
                         const SizedBox(height: 32),
                         Align(
                           alignment: Alignment.centerRight,
-                          child: Button(
+                          child: _isCurrentPinVerified ?
+                          Button(
                             label: "Update PIN",
                             variant: ButtonVariant.primary,
-                            heroIcon: HeroIcons.check,
-                            onPressed: () {} // TO DO: Logic for updating PIN
-                          ),
+                            icon: Icons.check,
+                            onPressed: () {
+                            // TODO: validate new == confirm, then save to DB
+                            if (_newPinController.text != _confirmPinController.text){
+                              StatusToast.show(context, 
+                              title: 'PIN Mismatch', 
+                              message: 'New PIN and Confirm PIN do not match.', 
+                              isSuccess: false,
+                              );
+                              return;
+                            }
+                            //TODO: save new PIN to DB
+                              StatusToast.show(context, 
+                              title: 'PIN Updated', 
+                              message: 'Your login PIN has been updated successfully.', 
+                              isSuccess: true,
+                              );
+                            },
+                          )
+                          : Button (
+                            label: 'Verify PIN',
+                            variant: ButtonVariant.secondary,
+                            icon: Icons.lock_open_outlined,
+                            onPressed: _verifyCurrentPin,
+                          )
                         ),
-                     ],
+                    ],
                   ),
               ],
             ),
           ),
-        ),
+          )
+        ],
       ),
-    );
-  }
+    ),
+  );
+}
 }
