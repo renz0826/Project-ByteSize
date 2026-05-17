62f84adf9039bb9462ee1c76f467747f7a082823
renz0826 <brenzbedonia@gmail.com>
2026-05-17 21:50:57 +0800

    fix: constant clinicalstaff companion

--- app/lib/db/database.dart
+++ app/lib/db/database.dart
@@ -8,22 +8,32 @@ import 'package:drift/drift.dart' as drift;
 
 part 'database.g.dart'; // connect database.g.dart here
 
-@DriftDatabase(tables: [Patient, ClinicalStaff, Appointment, Invoice, ProcedureCharge, PaymentTransaction, ClinicalRecord ])
-class AppDatabase extends _$AppDatabase { // registers all entity tables and attributes
+@DriftDatabase(tables: [
+  Patient,
+  ClinicalStaff,
+  Appointment,
+  Invoice,
+  ProcedureCharge,
+  PaymentTransaction,
+  ClinicalRecord
+])
+class AppDatabase extends _$AppDatabase {
+  // registers all entity tables and attributes
   AppDatabase() : super(_openConnection());
 
   @override
   int get schemaVersion => 1;
-  
+
   @override
-  MigrationStrategy get migration => MigrationStrategy( // adds the data in the table immediately in first startup
+  MigrationStrategy get migration => MigrationStrategy(
+        // adds the data in the table immediately in first startup
         onCreate: (Migrator m) async {
           // 1. Create all the tables first
           await m.createAll();
 
           // 2. Insert the default Dr. Reynaldo Tu profile immediately
           await into(clinicalStaff).insert(
-            const ClinicalStaffCompanion(
+            ClinicalStaffCompanion(
               staffId: drift.Value(1),
               firstName: drift.Value('Reynaldo'),
               lastName: drift.Value('Tu'),
@@ -34,12 +44,11 @@ class AppDatabase extends _$AppDatabase { // registers all entity tables and att
       );
 }
 
-LazyDatabase _openConnection() { // create the 'dentcity' file in the documents file
+LazyDatabase _openConnection() {
+  // create the 'dentcity' file in the documents file
   return LazyDatabase(() async {
     final dir = await getApplicationDocumentsDirectory();
     final file = File(p.join(dir.path, 'dentcity.db'));
     return NativeDatabase(file);
   });
 }
-
-
